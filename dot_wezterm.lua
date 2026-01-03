33aec9623a3f86140b777ea9f54cfc614a44a30d
TheLionCoder <eorlando.reyesruiz@gmail.com>
2026-01-03 16:33:27 -0500

    remove tmux and wezterm configurations

--- dot_wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-
-config.color_scheme = "GruvboxDark"
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 14
-config.window_background_opacity = 0.88
-
-return config
